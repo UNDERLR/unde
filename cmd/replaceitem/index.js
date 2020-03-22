751b841d2a77c9c05b9e6330bf8bcb7251e7a3b5
UNDERLR <47629011+UNDERLR@users.noreply.github.com>
2020-03-22 22:40:49 +0800

    Update index.js

--- cmd/replaceitem/index.js
+++ cmd/replaceitem/index.js
@@ -32,7 +32,7 @@ $(function () {
         this.value = this.value.replace(/^\s|\s{2}$|[^0-9~\-\.\s^]/g, '');
         let val = this.value;
         let points = val.split(' ');
-        if (!points[0]) points[0] = '~';pqapaap
+        if (!points[0]) points[0] = '~';
         if (!points[1]) points[1] = '~';
         if (!points[2]) points[2] = '~';
         $(this).autocomplete({
@@ -56,15 +56,111 @@ $(function () {
         }
     });
     $('.tar').keyup(function(){
-        let val = this.value.replace(/\s/,'');
+    let val = this.value.replace(/\s/,'');
+    let tars = [
+        "x",
+        "y",
+        "z",
+        "dx",
+        "dy",
+        "dz",
+        "r",
+        "rm",
+        "rx",
+        "rxm",
+        "ry",
+        "rym",
+        "scores",
+        "tag",
+        "c",
+        "l",
+        "lm",
+        "m",
+        "name",
+        "type"
+        ];
+        let tartip = [
+        "x点",
+        "y点",
+        "z点",
+        "dx-x轴长度",
+        "dy-y轴长度",
+        "dz-z轴长度",
+        "r-最大范围",
+        "rm-最小范围",
+        "rx-最大水平旋转角度",
+        "rxm-最小水平旋转角度",
+        "ry-最大垂直旋转角度",
+        "rym-最小垂直旋转角度",
+        "scores-记分板",
+        "tag-标签",
+        "c-数量",
+        "l-最大经验等级",
+        "lm-最小经验等级",
+        "m-模式",
+        "name-名字",
+        "type-生物类型"
+        ];
+    let target = [];
+    for (let i=0;i<tars.length;i++) {
+        let tar = val.substring(3,val.length);
+        let t = tar.match(/([^,]+=[^,]+,?)*/g);
+        target[i] = {label:val.match(/^@.{2}/) + t + tartip[i],value:val.match(/^@.{2}/) + t + tars[i] + '='};
+    }
+    if (/\[/.test(val) && /\]/.test(val) === false) {
+        $(this).autocomplete({
+            delay: 0,
+            source: target,
+            minLength: -1,
+            position: { my: 'left bottom>', at: 'left top' }
+        });
+    }else{
         $(this).autocomplete({
             delay: 0,
             source: [{label:'@a-所有玩家',value:'@a'},{label:'@p-最近的玩家',value:'@p'},{label:'@r-随机玩家',value:'@r'},{label:'@s-自己',value:'@s'},{label:'@e-所有实体',value:'@e'}],
             minLength: -1,
             position: { my: 'left bottom', at: 'left top' }
         });
+    }
+    if (/^@/.test(val)) {
+        if (/(x=[^\d.-]+|y=[^\d.-]+|z=[^\d.-]+|dx=[^\d.-]+|dy^=[^\d.-]+|dz=[^\d.-]+|r=[^\d.-]+|rm=[^\d.-]+|rx=[^\d.-]+|rxm=[^\d.-]+|ry=[^\d.-]+|rym=[^\d.-]+|l=[^\d.-]+|lm=[^\d.-]+|c=[^\d.-]+)/.test(val)) {
+            let et = val.match(/(x=[^~\d.-]+|y=[^~\d.-]+|z=[^~\d.-]+|dx=[^\d.-]+|dy^=[^\d.-]+|dz=[^\d.-]+|r=[^\d.-]+|rm=[^\d.-]+|rx=[^\d.-]+|rxm=[^\d.-]+|ry=[^\d.-]+|rym=[^\d.-]+|l=[^\d.-]+|lm=[^\d.-]+|c=[^\d.-]+)/g);
+            $('input:hover+div').html(`<i class='fad fa-times-square fa-fw'></i>错误:错误的"${et}"<pre>\n *= \n   ^此处应为数字或~</pre>`).css('border', '#ff6b81 1vw solid').css('color', '#ff4757');
+        } else if (/^@(a|s|p|e|r)\[[^\]]*\]/g.test(val) === false && /\[/g.test(val)) {
+            $('input:hover+div').html(`<i class='fad fa-times-square fa-fw'></i>错误:错误的"[",缺少"]"`).css('border', '#ff6b81 1vw solid').css('color', '#ff4757');
+        } else if (/^@(a|s|p|e|r)\[[^\[{]*{[^\]}]*}[^\[{]*\]/g.test(val) === false && /{/g.test(val)) {
+            $('input:hover+div').html(`<i class='fad fa-times-square fa-fw'></i>错误:错误的"{",缺少"}"`).css('border', '#ff6b81 1vw solid').css('color', '#ff4757');
+        } else if (/^@[^asper]{1}/g.test(val)) {
+            let et = val.match(/^@.*/g);
+            $('input:hover+div').html(`<i class='fad fa-times-square fa-fw'></i>错误:错误的${et},没有${et}选择器`).css('border', '#ff6b81 1vw solid').css('color', '#ff4757');
+        } else {
+            $('input:hover+div').html('<i class="fad fa-check-square fa-fw"></i>没有任何问题').css('border', '').css('color', '');
+        }
+    }
+});
+    $('.type').keyup(function(){
+        let val = this.value.replace(/\s/,'');
+        $(this).autocomplete({
+            delay: 0,
+            source: [
+                {label:'slot.armor-马铠',value:'slot.armor'},
+                {label:'slot.armor.chest-护甲',value:'slot.armor.chest'},
+                {label:'slot.armor.feet-靴子',value:'slot.armor.feet'},
+                {label:'slot.armor.head-头盔',value:'slot.armor.head'},
+                {label:'slot.armor.legs-腿甲',value:'slot.armor.legs'},
+                {label:'slot.weapon.mainhand-主手',value:'weapon.mainhand'},
+                {label:'slot.weapon.offhand-副手',value:'slot.weapon.offhand'},
+                {label:'slot.chest-箱子',value:'slot.chest'},
+                {label:'slot.enderchest-末影箱',value:'slot.enderchest'},
+                {label:'slot.hotbar-快捷栏',value:'slot.hotbar'},
+                {label:'slot.inventory-物品栏',value:'slot.inventory'},
+                {label:'slot.saddle-鞍',value:'slot.saddle'}
+                ],
+            minLength: -1,
+            position: { my: 'left bottom', at: 'left top' }
+        });
         if (/^@/.test(val)) {
-            if (/^@[^asper]/g.test(val)) {
+            if (/^@(asper)/g.test(val)) {
                 let et = val.match(/^@.*/g);
                 $('input:hover+div').html(`<i class='fad fa-times-square fa-fw'></i>错误:错误的${et}`).css('border', '#ff6b81 1vw solid').css('color', '#ff4757');
             } else {
@@ -72,7 +168,7 @@ $(function () {
             }
         }
     });
-    $('.blocks').keyup(function () {
+    $('.items').keyup(function () {
         this.value = this.value.replace(/[^a-z0-9_:\-]/g, '');
         let val = this.value;
         $(this).autocomplete({
@@ -120,4 +216,3 @@ $(function () {
         alert('复制成功');
     });
 });
-    
