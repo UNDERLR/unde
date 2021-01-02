eac291be22076e0abdb09ce01761275946a2ec77
UNDERLR <47629011+UNDERLR@users.noreply.github.com>
2021-01-02 22:27:44 +0800

    Add files via upload

--- yuanshen/index.js
+++ yuanshen/index.js
@@ -13,17 +13,20 @@ function launchIntoFullscreen(element) {
 function ranA(a) {
     return a[Math.floor(Math.random() * a.length)];
 }
+let t = 1;
 
 function ran(times) {
     let out = [];
-    let t = 1;
     let fi = 0;
     let fo = 0;
+    t += times;
+
     for (let i = 0; i < times; i++) {
         if (t % 90 === 0) {
             fi += 1;
             out.push(ranA(["刻晴", "莫娜", "七七", "迪卢克", "琴", "阿莫斯之弓", "天空之翼", "四风原典", "天空之卷", "和璞鸢", "天空之脊", "狼的末路", "天空之傲"]));
-        } else if (t % 10 === 0) {
+        }
+        if (t % 10 === 0) {
             fo += 1;
             out.push(ranA(["辛焱", "砂糖", "迪奥娜", "重云", "诺艾尔", "班尼特", "菲谢尔", "凝光", "行秋", "北斗", "香菱", "安柏", "雷泽", "凯亚", "芭芭拉", "丽莎", "弓藏", "祭礼弓", "绝弦", "西风猎弓", "昭心", "祭礼残章", "流浪乐章", "西风秘典", "西风长枪", "匣里灭辰", "雨裁", "祭礼大剑", "钟剑", "西风大剑", "匣里龙吟", "祭礼剑", "笛剑", "西风剑"]));
         } else {
@@ -53,19 +56,42 @@ function ran(times) {
 
     return out;
 }
+function skip() {
+    document.querySelector(".videoBox").style.display = "none";
+    document.querySelector(".video video").pause();
+    document.querySelector(".video video").currentTime = 0;
+    console.log("播放结束");
+    document.querySelectorAll(".vi>div").forEach(function (e) {
+        e.addEventListener("mouseover", function () {
+            document.querySelector(".vi>div audio").play();
+        });
+        e.addEventListener("mousedown", function () {
+            document.querySelector(".vi").removeChild(e);
+        });
+    });
+}
 
 function display(a, t) {
     let vBox = document.querySelector(".video");
-    let video = document.querySelector(".video>video");
+    let video = document.querySelector(".video video");
+    let videoBox = document.querySelector(".videoBox");
     vBox.style.display = "block";
-    video.style.display = "block";
+    videoBox.style.display = "block";
 
     let v = 3;
     let ty = "单";
 
     function end() {
-        video.style.display = "none";
+        videoBox.style.display = "none";
         console.log("播放结束");
+        document.querySelectorAll(".vi>div").forEach(function (e) {
+            e.addEventListener("mouseover", function () {
+                document.querySelector(".vi>div audio").play();
+            });
+            e.addEventListener("mousedown", function () {
+                document.querySelector(".vi").removeChild(e);
+            });
+        });
     }
 
     for (let i = 0; i < a.length; i++) {
@@ -73,20 +99,36 @@ function display(a, t) {
         let div = document.createElement("div");
         let img = document.createElement("img");
         if (link["fi"][t]) {
+            let s = "";
             img.src = link["fi"][t];
             if (v < 5) v = 5;
+            div.className = "fi";
+            s = "五星";
         } else if (link["fo"][t]) {
             if (v < 4) v = 4;
+            div.className = "fo";
             img.src = link["fo"][t];
+            s = "四星";
         } else if (link["th"][t]) {
             img.src = link["th"][t];
+            s = "三星";
+            div.className = "th";
         }
 
-        div.innerHTML = t;
+        let au = document.createElement("audio");
+        let so = document.createElement("source");
+        so.src = `./sound/${s}.mp3`;
+        au.appendChild(so);
+        let p = document.createElement("div");
+        p.innerHTML = t;
+        div.onclick = function () {
+            alert;
+        };
+        div.appendChild(p);
         div.appendChild(img);
-        div.onclick = "this.parentNode.removeChild(this);";
+        div.appendChild(au);
 
-        document.querySelector(".video>.res").appendChild(div);
+        document.querySelector(".video>.vi").appendChild(div);
     }
     switch (v) {
         case 5:
@@ -111,12 +153,12 @@ function display(a, t) {
     }
 
     console.log(`${v}-${ty}`);
-    document.querySelector(".video>video").src = `./video/${v}-${ty}.mp4`;
+    document.querySelector(".video video").src = `./video/${v}-${ty}.mp4`;
 
-    setTimeout(`document.querySelector(".video>video").play()`, 500);
+    setTimeout(`document.querySelector(".video video").play()`, 500);
     video.addEventListener("ended", () => end());
+    document.querySelector(".res").innerHTML = document.querySelector(".video>.vi").innerHTML;
 }
-
 function wish(times, type) {
     switch (type) {
         case 3:
