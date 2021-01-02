ba8f270b74e6e7241b1cb03554b5d53dd3e8de18
UNDERLR <47629011+UNDERLR@users.noreply.github.com>
2021-01-02 15:35:47 +0800

    Add files via upload

--- yuanshen/index.js
+++ yuanshen/index.js
@@ -1,25 +1,14 @@
-let a = function () {
-    function launchIntoFullscreen(element) {
-        if (element.requestFullscreen) {
-            element.requestFullscreen();
-        } else if (element.mozRequestFullScreen) {
-            element.mozRequestFullScreen();
-        } else if (element.webkitRequestFullscreen) {
-            element.webkitRequestFullscreen();
-        } else if (element.msRequestFullscreen) {
-            element.msRequestFullscreen();
-        }
+function launchIntoFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
     }
-    document.documentElement.addEventListener(
-        "mousedown",
-        () =>
-            function () {
-                launchIntoFullscreen(document.documentElement);
-            }
-    );
-};
-
-a();
+}
 
 function ranA(a) {
     return a[Math.floor(Math.random() * a.length)];
@@ -33,28 +22,28 @@ function ran(times) {
     for (let i = 0; i < times; i++) {
         if (t % 90 === 0) {
             fi += 1;
-            out.push("五星" + ranA(["角色刻晴", "角色莫娜", "角色七七", "角色迪卢克", "角色琴", "武器阿莫斯之弓", "武器天空之翼", "武器四风原典", "武器天空之卷", "武器和璞鸢", "武器天空之脊", "武器狼的末路", "武器天空之傲"]));
+            out.push(anA(["刻晴", "莫娜", "七七", "迪卢克", "琴", "阿莫斯之弓", "天空之翼", "四风原典", "天空之卷", "和璞鸢", "天空之脊", "狼的末路", "天空之傲"]));
         } else if (t % 10 === 0) {
             fo += 1;
-            out.push("四星" + ranA(["角色辛焱", "角色砂糖", "角色迪奥娜", "角色重云", "角色诺艾尔", "角色班尼特", "角色菲谢尔", "角色凝光", "角色行秋", "角色北斗", "角色香菱", "角色安柏", "角色雷泽", "角色凯亚", "角色芭芭拉", "角色丽莎", "武器弓藏", "武器祭礼弓", "武器绝弦", "武器西风猎弓", "武器昭心", "武器祭礼残章", "武器流浪乐章", "武器西风秘典", "武器西风长枪", "武器匣里灭辰", "武器雨裁", "武器祭礼大剑", "武器钟剑", "武器西风大剑", "武器匣里龙吟", "武器祭礼剑", "武器笛剑", "武器西风剑"]));
+            out.push(ranA(["辛焱", "砂糖", "迪奥娜", "重云", "诺艾尔", "班尼特", "菲谢尔", "凝光", "行秋", "北斗", "香菱", "安柏", "雷泽", "凯亚", "芭芭拉", "丽莎", "弓藏", "祭礼弓", "绝弦", "西风猎弓", "昭心", "祭礼残章", "流浪乐章", "西风秘典", "西风长枪", "匣里灭辰", "雨裁", "祭礼大剑", "钟剑", "西风大剑", "匣里龙吟", "祭礼剑", "笛剑", "西风剑"]));
         } else {
             let a = Math.random();
             if (a >= 0.994) {
                 fi += 1;
                 if (a >= 0.997) {
-                    out.push("五星" + ranA(["角色刻晴", "角色莫娜", "角色七七", "角色迪卢克", "角色琴"]));
+                    out.push(ranA(["刻晴", "莫娜", "七七", "迪卢克", "琴"]));
                 } else if (a <= 0.997 && a >= 0.994) {
-                    out.push("五星" + ranA(["武器阿莫斯之弓", "武器天空之翼", "武器四风原典", "武器天空之卷", "武器和璞鸢", "武器天空之脊", "武器狼的末路", "武器天空之傲"]));
+                    out.push(ranA(["阿莫斯之弓", "天空之翼", "四风原典", "天空之卷", "和璞鸢", "天空之脊", "狼的末路", "天空之傲"]));
                 }
             } else if (a <= 0.994 && a >= 0.943) {
                 fo += 1;
                 if (a >= 0.9685) {
-                    out.push("四星" + ranA(["角色辛焱", "角色砂糖", "角色迪奥娜", "角色重云", "角色诺艾尔", "角色班尼特", "角色菲谢尔", "角色凝光", "角色行秋", "角色北斗", "角色香菱", "角色安柏", "角色雷泽", "角色凯亚", "角色芭芭拉", "角色丽莎"]));
+                    out.push(ranA(["辛焱", "砂糖", "迪奥娜", "重云", "诺艾尔", "班尼特", "菲谢尔", "凝光", "行秋", "北斗", "香菱", "安柏", "雷泽", "凯亚", "芭芭拉", "丽莎"]));
                 } else if (a <= 0.9685 && a >= 0.943) {
-                    out.push("四星" + ranA(["武器弓藏", "武器祭礼弓", "武器绝弦", "武器西风猎弓", "武器昭心", "武器祭礼残章", "武器流浪乐章", "武器西风秘典", "武器西风长枪", "武器匣里灭辰", "武器雨裁", "武器祭礼大剑", "武器钟剑", "武器西风大剑", "武器匣里龙吟", "武器祭礼剑", "武器笛剑", "武器西风剑"]));
+                    out.push(ranA(["弓藏", "祭礼弓", "绝弦", "西风猎弓", "昭心", "祭礼残章", "流浪乐章", "西风秘典", "西风长枪", "匣里灭辰", "雨裁", "祭礼大剑", "钟剑", "西风大剑", "匣里龙吟", "祭礼剑", "笛剑", "西风剑"]));
                 }
             } else {
-                out.push("三星" + ranA(["武器弹弓", "武器神射手之誓", "武器鸦羽弓", "武器翡玉法球", "武器讨龙英杰谭", "武器魔导绪论", "武器黑缨枪", "武器以理服人", "武器沐浴龙血的剑", "武器铁影阔剑", "武器飞天御剑", "武器黎明神剑", "武器冷刃"]));
+                out.push(ranA(["弹弓", "神射手之誓", "鸦羽弓", "翡玉法球", "讨龙英杰谭", "魔导绪论", "黑缨枪", "以理服人", "沐浴龙血的剑", "铁影阔剑", "飞天御剑", "黎明神剑", "冷刃"]));
             }
         }
         t += 1;
@@ -65,11 +54,14 @@ function ran(times) {
     return out;
 }
 
-function display(a) {
+function display(a, t) {
     let vBox = document.querySelector(".video");
     let video = document.querySelector(".video>video");
     vBox.style.display = "block";
     video.style.display = "block";
+
+    let v = 3;
+    let ty = "单";
 
     function end() {
         video.style.display = "none";
@@ -79,12 +71,49 @@ function display(a) {
     for (let i = 0; i < a.length; i++) {
         const t = a[i];
         let div = document.createElement("div");
+        let img = document.createElement("img");
+        if (link["fi"][t]) {
+            img.src = link["fi"][t];
+            if (v < 5) v = 5;
+        } else if (link["fo"][t]) {
+            if (v < 4) v = 4;
+            img.src = link["fo"][t];
+        } else if (link["th"][t]) {
+            img.src = link["th"][t];
+        }
+
         div.innerHTML = t;
+        div.appendChild(img);
+        div.onclick = "this.parentNode.removeChild(this);";
 
         document.querySelector(".video>.res").appendChild(div);
     }
+    switch (v) {
+        case 5:
+            v = "五星";
+            break;
+        case 4:
+            v = "四星";
+            break;
 
-    setTimeout(`document.querySelector(".video>video").play()`, 1000);
+        default:
+            v = "三星";
+            break;
+    }
+    switch (t) {
+        case 1:
+            ty = "单";
+            break;
+
+        default:
+            ty = "十";
+            break;
+    }
+
+    console.log(`${v}-${ty}`);
+    document.querySelector(".video>video").src = `./video/${v}-${ty}.mp4`;
+
+    setTimeout(`document.querySelector(".video>video").play()`, 500);
     video.addEventListener("ended", () => end());
 }
 
@@ -92,7 +121,7 @@ function wish(times, type) {
     switch (type) {
         case 3:
             let res = ran(times);
-            display(res);
+            display(res, times);
             console.log(res);
             break;
 
