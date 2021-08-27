2a919262b153c18a4e845ffdc721d1d74fb8a6eb
UNDERLR <47629011+UNDERLR@users.noreply.github.com>
2021-08-27 19:59:35 +0800

    Add files via upload

--- cmd/cmd/index.js
+++ cmd/cmd/index.js
@@ -1,165 +1,211 @@
-let block;
-let player;
-(function(){
-    $('button').click(function(){
-        let txt = $('input').val();
-        exe(txt);
-        $('input').val('');
-    });
-    player = {
-        x: 0,
-        y: 0,
-        z: 0,
-        armor: {
-            head: 'none',
-            cheat: 'none',
-            leg: 'none',
-            foot: 'none'
-        },
-        offer: 'none',
-        
+let valueSplitArray;
+
+document.getElementById("input").addEventListener("input", (e) => {
+    let inputView = document.getElementById("inputView");
+    let inputTip = document.getElementById("inputTip");
+    let tipSpan = document.getElementById("span");
+    let tipDisplayUl = document.querySelector("#tipDisplay>ul");
+    let value = e.target.value;
+    // console.log(e);
+    let valueSplit = value.split(" ");
+    valueSplitArray = () => {
+        let arr = () => {
+            return valueSplit.toString();
+        };
+        return arr();
     };
-    block = {};
-}());
-function exe(t) {
-    let txt = t.replace(/^\/|\s.*/g,'');
-    if (/(?<=blocks\.).+/.test(txt)){
-        let bl = t.match(/(?<=blocks\.).+/);
-        output(t,JSON.stringify(block[bl]));
-        return;
-    }
-    switch(txt) {
-        case 'clear':
-            $('.output').html('');
-            break;
-        case 'blocks':
-            output(t,JSON.stringify(block));
-            break;
-        case 'player':
-            output(t,JSON.stringify(player));
-            break;
-        case 'reset':
-            block = {};
-            break;
-        case 'tp':
-            if (/tp(\s+-?\d+){3}/.test(t)) {
-                let x = t.match(/(?<=tp(\s+-?\d+){0}\s+)-?\d+/g)*1;
-                let y = t.match(/(?<=tp(\s+-?\d+){1}\s+)-?\d+/g)*1;
-                let z = t.match(/(?<=tp(\s+-?\d+){2}\s+)-?\d+/g)*1;
-                player.x = x;
-                player.y = y;
-                player.z = z;
-                output(t,`已将玩家传送至 ${x} ${y} ${z}`);
+    inputView.innerHTML = `<pre>${value}</pre>`;
+
+    // inputTip.innerHTML = value + "<span>asda</span>";
+
+    for (let i = 0; i < valueSplit.length; i++) {
+        tipDisplayUl.innerHTML = "";
+        if (valueSplit.length === 1) {
+            for (const Key in grammar) {
+                let k = Key;
+                if (/^\//.test(valueSplit[0])) k = "/" + k;
+                tipSpan.innerHTML = "";
+                let rxp = new RegExp(valueSplit[0], "g");
+                if (rxp.test(k)) {
+                    let li = document.createElement("li");
+                    //是否可被自动完成
+                    li.setAttribute("completable", true);
+
+                    li.setAttribute("value", k);
+                    li.innerHTML =
+                        k.replace(rxp, (t) => {
+                            if (k === t) {
+                                inputView.innerHTML = `<span style="color: #AAAAAA">${k}</span>`;
+                                document.querySelector(".autoOpenWiki>iframe").src = `https://wiki.biligame.com/mc/%E5%91%BD%E4%BB%A4/${valueSplit[0].replace("/", "")}`;
+                            }
+                            return `<span style="font-weight: lighter;color: hsl(0,0%,60%)">${t}</span>`;
+                        }) +
+                        " - " +
+                        grammar[Key].text;
+                    i++;
+
+                    tipDisplayUl.append(li);
+                }
             }
-            break;
-        case 'say':
-            let o = function() {
-                if (/\s/g.test(t)) {
-                    return(t.match(/\s.*/));
-                }else return(' error at "say<=" (is not has string)');                                                                                                            
-            };
-            output(t,o());
-            break;
-        case 'fill':
-            let bs = function(){
-                if (/fill(\s+-?\d+){6}\s+\S+/.test(t)) {
-                    let p1 = {  
-                        x : t.match(/(?<=\/?fill\s+(-?\d+\s+){0})-?\d+/g),
-                        y : t.match(/(?<=\/?fill\s+(-?\d+\s+){1})-?\d+/g),
-                        z : t.match(/(?<=\/?fill\s+(-?\d+\s+){2})-?\d+/g)
-                    };
-                    let p2 = {  
-                        x : t.match(/(?<=\/?fill\s+(-?\d+\s+){3})-?\d+/g),
-                        y : t.match(/(?<=\/?fill\s+(-?\d+\s+){4})-?\d+/g),
-                        z : t.match(/(?<=\/?fill\s+(-?\d+\s+){5})-?\d+/g)
-                    };
-                    let id = t.match(/(?<=fill(\s+-?\d+){6}\s+)\S+/g);
-                    let x,y,z;
-                    p1.x > p2.x ? x = p1.x - p2.x: x = p2.x - p1.x;
-                    p1.y > p2.y ? y = p1.y - p2.y: y = p2.y - p1.y;
-                    p1.z > p2.z ? z = p1.z - p2.z: z = p2.z - p1.z;
-                    let blocks = (x + 1)*(y + 1)*(z + 1);
-                    if (blocks > 32768) {
-                        return(`${p1.x},${p1.y},${p1.z} to ${p2.x},${p2.y},${p2.z} blocks(${blocks}) > 32767 (if you want to fill the blocks > 32768,please use "filln")`);
-                    } else {
-                        let mx,my,mz;
-                        p1.x < p2.x ? mx = p1.x*1:mx = p2.x*1;
-                        p1.y < p2.y ? my = p1.y*1:my = p2.y*1;
-                        p1.z < p2.z ? mz = p1.z*1:mz = p2.z*1;
-                        
-                        for (let by = 0;by<=y;by++) {
-                            for (let bx = 0;bx<=x;bx++) {
-                                for (let bz = 0;bz<=z;bz++) {
-                                    let b = {id:id,x:bx+mx,y:by+my,z:bz+mz};
-                                    block[`b${bx+mx}-${by+my}-${bz+mz}`] = b;
-                                }
-                            }
-                        }
-                        return(`${blocks} blocks was been changed`);
+        } else {
+            let cmd = valueSplit[0].replace("/", "");
+            let gra = grammar[cmd]["grammar"];
+            let gras = typeof gra !== "string" ? gra().split(" ") : gra.split(" ");
+            gras.shift();
+            let nowTip = gras[valueSplit.length - 2];
+
+            if (/^<|>$/g.test(gras[valueSplit.length - 2])) nowTip = variate[nowTip.replace(/^<|>$/g, "")];
+            if (/^\[|]$/g.test(gras[valueSplit.length - 2])) nowTip = nowTip.replace(/^\[|]$/g, "").split("|");
+
+            let spanValue = "";
+            for (let j = 0; j < valueSplit.length - 1; j++) {
+                spanValue += valueSplit[j] + " ";
+            }
+            tipSpan.innerHTML = `<pre>${spanValue}</pre>`;
+
+            if (typeof nowTip === "string") {
+                console.log(nowTip);
+                let k = nowTip;
+                let li = document.createElement("li");
+                //是否可被自动完成
+                li.setAttribute("completable", false);
+
+                li.setAttribute("value", k);
+                li.innerHTML = nowTip;
+                tipDisplayUl.append(li);
+            } else if (Array.isArray(nowTip) === false) {
+                for (let j in nowTip) {
+                    let k = j;
+                    let rxp = new RegExp(valueSplit[valueSplit.length - 1], "g");
+                    if (rxp.test(k)) {
+                        let li = document.createElement("li");
+                        //是否可被自动完成
+                        li.setAttribute("completable", true);
+
+                        li.setAttribute("value", k);
+                        li.innerHTML =
+                            k.replace(rxp, (t) => {
+                                return `<span style="font-weight: bolder;color: #AAAAAA">${t}</span>`;
+                            }) + ` - ${nowTip[j]}`;
+                        tipDisplayUl.append(li);
                     }
-                }else return('error');
-            };
-            output(t,bs());
-            break;
-        case 'filln':
-            let bls = function(){
-                if (/filln(\s+-?\d+){6}\s+\S+/.test(t)) {
-                    let p1 = {  
-                        x : t.match(/(?<=\/?filln\s+(-?\d+\s+){0})-?\d+/g),
-                        y : t.match(/(?<=\/?filln\s+(-?\d+\s+){1})-?\d+/g),
-                        z : t.match(/(?<=\/?filln\s+(-?\d+\s+){2})-?\d+/g)
-                    };
-                    let p2 = {  
-                        x : t.match(/(?<=\/?filln\s+(-?\d+\s+){3})-?\d+/g),
-                        y : t.match(/(?<=\/?filln\s+(-?\d+\s+){4})-?\d+/g),
-                        z : t.match(/(?<=\/?filln\s+(-?\d+\s+){5})-?\d+/g)
-                    };
-                    let id = t.match(/(?<=fill(\s+-?\d+){6}\s+)\S+/g);
-                    let x,y,z;
-                    p1.x > p2.x ? x = p1.x - p2.x: x = p2.x - p1.x;
-                    p1.y > p2.y ? y = p1.y - p2.y: y = p2.y - p1.y;
-                    p1.z > p2.z ? z = p1.z - p2.z: z = p2.z - p1.z;
-                    let blocks = (x + 1)*(y + 1)*(z + 1);
-                    
-                    let mx,my,mz;
-                    p1.x < p2.x ? mx = p1.x*1:mx = p2.x*1;
-                    p1.y < p2.y ? my = p1.y*1:my = p2.y*1;
-                    p1.z < p2.z ? mz = p1.z*1:mz = p2.z*1;
-                        
-                    for (let by = 0;by<=y;by++) {
-                        for (let bx = 0;bx<=x;bx++) {
-                            for (let bz = 0;bz<=z;bz++) {
-                                let b = {id:id,x:bx+mx,y:by+my,z:bz+mz};
-                                block[`b${bx+mx}-${by+my}-${bz+mz}`] = b;
-                            }
+                }
+            } else if (Array.isArray(nowTip) === true) {
+                for (let x = 0; x < nowTip.length; x++) {
+                    nowTip[x] = JSON.parse('{"' + nowTip[x].replace(/'/g, '"').replace(/:/g, '":') + "}");
+                    for (let j in nowTip[x]) {
+                        let k = j;
+                        let rxp = new RegExp(valueSplit[valueSplit.length - 1], "g");
+                        if (rxp.test(k)) {
+                            let li = document.createElement("li");
+                            //是否可被自动完成
+                            li.setAttribute("completable", true);
+
+                            li.setAttribute("value", k);
+                            li.innerHTML =
+                                k.replace(rxp, (t) => {
+                                    return `<span style="font-weight: bolder;color: #AAAAAA">${t}</span>`;
+                                }) + ` - ${nowTip[x][j]}`;
+                            tipDisplayUl.append(li);
                         }
                     }
-                    return(`${blocks} blocks was been changed`);
-                }else return('error');
-            };
-            output(t,bls());
-            break;
-        default:
-            output(t,t);
+                }
+            }
+        }
+
+        if (tipDisplayUl.childElementCount < 1) {
+            tipDisplayUl.style.opacity = 0;
+        } else tipDisplayUl.style.opacity = 1;
+
+        tipDisplayUl.firstElementChild.classList.add("selected");
     }
-}
-function output(c,o) {
-    $('.output').append(`<div class='re'>
-                    <div class='in'>
-                        <div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                        <span>${c}</span>
-                    </div>
-                    <div class='ou'>
-                        <div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                        <span>${o}</span>
-                    </div>
-                </div>`);
-}
+
+    function view(t) {
+        let v = "";
+        let tip = t;
+
+        if (valueSplit.length < 2) {
+            v = e.target.value;
+            tip = tip.replace("/", "");
+        } else {
+            for (let i = 0; i < valueSplit.length - 1; i++) {
+                v += valueSplit[i] + " ";
+            }
+            let rxp = new RegExp("^" + valueSplit[valueSplit.length - 1]);
+            tip = tip.replace(rxp, "");
+        }
+        // for (let i = 0; i < valueSplit[valueSplit.length - 1].length; i++) {
+        //     if (t[i]!==valueSplit[valueSplit.length - 1][i]){
+        //         tip="";
+        //         return
+        //     }else {
+        //         tip = t.replace(valueSplit[valueSplit.length - 1], "")
+        //         v+=valueSplit[valueSplit.length - 1];
+        //     }
+        //
+        // }
+
+        // inputView.innerHTML = `<span style="color: white;"><pre>${v}</pre></span>` + `<span style="color: hsl(0,0%,30%);">${tip}</span>`;
+    }
+
+    let selectedLi = document.getElementsByClassName("selected")[0];
+    view(selectedLi.getAttribute("value"));
+    document.body.onkeydown = (e) => {
+        selectedLi = document.getElementsByClassName("selected")[0];
+        switch (e.key) {
+            case "ArrowDown":
+                function down() {
+                    if (selectedLi !== tipDisplayUl.lastElementChild) {
+                        selectedLi.classList.remove("selected");
+                        selectedLi.nextElementSibling.classList.add("selected");
+                        view(selectedLi.nextElementSibling.getAttribute("value"));
+                        tipDisplayUl.scrollTo(0, selectedLi.offsetTop - selectedLi.offsetHeight * 3);
+                    } else {
+                        selectedLi.classList.remove("selected");
+                        tipDisplayUl.firstElementChild.classList.add("selected");
+                        view(tipDisplayUl.firstElementChild.getAttribute("value"));
+                        tipDisplayUl.scrollTo(0, tipDisplayUl.firstElementChild.offsetTop - tipDisplayUl.firstElementChild.offsetHeight * 2);
+                    }
+                }
+
+                down();
+                break;
+            case "ArrowUp":
+                function up() {
+                    if (selectedLi !== tipDisplayUl.firstElementChild) {
+                        selectedLi.classList.remove("selected");
+                        selectedLi.previousElementSibling.classList.add("selected");
+                        view(selectedLi.previousElementSibling.getAttribute("value"));
+                        tipDisplayUl.scrollTo(0, selectedLi.offsetTop - selectedLi.offsetHeight * 5);
+                        tipDisplayUl.scrollTo(0, selectedLi.offsetTop - selectedLi.offsetHeight * 5);
+                    } else {
+                        selectedLi.classList.remove("selected");
+                        tipDisplayUl.lastElementChild.classList.add("selected");
+                        view(tipDisplayUl.lastElementChild.getAttribute("value"));
+                        tipDisplayUl.scrollTo(0, tipDisplayUl.lastElementChild.offsetTop - tipDisplayUl.lastElementChild.offsetHeight * 4);
+                    }
+                }
+
+                up();
+                break;
+            case "Tab":
+                if (selectedLi.getAttribute("completable") === "true") {
+                    if (valueSplit.length < 2) {
+                        document.querySelector(".autoOpenWiki>iframe").src = `https://wiki.biligame.com/mc/%E5%91%BD%E4%BB%A4/${selectedLi.getAttribute("value").replace("/", "")}`;
+                        e.target.value = inputView.innerHTML = selectedLi.getAttribute("value");
+                    } else {
+                        let v = "";
+                        for (let i = 0; i < valueSplit.length - 1; i++) {
+                            v += valueSplit[i] + " ";
+                        }
+                        e.target.value = inputView.innerHTML = v + selectedLi.getAttribute("value");
+                    }
+                } else down();
+                break;
+        }
+    };
+});
+
+document.getElementById("input").addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "Tab") e.preventDefault();
+});
